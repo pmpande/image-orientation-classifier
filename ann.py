b87720d0967fa3feda2d692c79af6b32eb5505f0
nishshah <nishshah@indiana.edu>
2015-12-13 03:50:28 -0500

    Normalised inputs and bug fix
    Divided inputs by 255 to make is smaller in number and also fixed get
    output vector bug

--- ann.py
+++ ann.py
@@ -1,5 +1,6 @@
 import time
 import math
+from sys import exit
 #http://arctrix.com/nas/python/bpnn.py
 
 
@@ -18,17 +19,18 @@ def summation2(w, inp, no, minimum, maximum):
 
 
 def get_output_vector(value):
-    if value == '0':
+    if value[1] == '0':
         return [1.0, 0, 0, 0]
-    if value == '90':
+    if value[1] == '90':
         return [0, 1.0, 0, 0]
-    if value == '180':
+    if value[1] == '180':
         return [0, 0, 1.0, 0]
     else:
         return [0, 0, 0, 1.0]
 
 
 def g(val):
+    #print 1/(1 + math.exp(-val))
     return 1.0/(1.0 + math.exp(-val))
 
 
@@ -39,10 +41,10 @@ def g_dash(val):
 def initialize_weights(w, no_input_nodes, no_hidden_nodes, no_output_nodes):
     for x in range(no_input_nodes):
         for y in range(no_hidden_nodes):
-            w[(x, y + no_input_nodes)] = 1.0
+            w[(x, y + no_input_nodes)] = 0.5
     for x in range(no_hidden_nodes):
         for y in range(no_output_nodes):
-            w[(x + no_input_nodes, y + no_input_nodes + no_hidden_nodes)] = 1.0
+            w[(x + no_input_nodes, y + no_input_nodes + no_hidden_nodes)] = 0.5
     return w
 
 
@@ -64,40 +66,52 @@ def ann(train_vector):
     a = {}
     w = initialize_weights(w, no_input_nodes, no_hidden_nodes, no_output_nodes)
     counter = 0
-    for k in range(10):
+    for k in range(1):
         cnt=0.0
-        for i in train_vector.keys()[1:30000]:
+        for i in train_vector.keys():
             counter += 1
-            output_vector = get_output_vector(train_vector[i][1])
+            output_vector = get_output_vector(i)
             inp = {}
             delta = {}
             #Forward Propogation
             for x in range(no_input_nodes):
-                a[x] = float(train_vector[i][1][x])
+                a[x] = float(train_vector[i][1][x])/255
+            #print a
+            #print train_vector[i][1]
+
             for unit in range(no_input_nodes, no_input_nodes + no_hidden_nodes):
-                inp[unit] = summation1(w, a, unit, 0, no_input_nodes)
+                inp[unit] = summation1(w, a, unit, 0, no_input_nodes) + 1
                 a[unit] = g(inp[unit])
+            #print inp
+            #print a
+            #exit()
             l = []
             for unit in range(no_input_nodes + no_hidden_nodes, no_input_nodes + no_hidden_nodes + no_output_nodes):
-                inp[unit] = summation1(w, a, unit, no_input_nodes, no_input_nodes + no_hidden_nodes)
+                inp[unit] = summation1(w, a, unit, no_input_nodes, no_input_nodes + no_hidden_nodes) + 1
                 a[unit] = g(inp[unit])
                 l.append(a[unit])
-            print l
+            #print l
             max12=l.index(max(l))
-            actual=output_vector.index(1)
+            actual=output_vector.index(1.0)
             if max12==actual:
                 cnt+=1
             #Back Propogation
             for unit in range(no_input_nodes + no_hidden_nodes, no_input_nodes + no_hidden_nodes + no_output_nodes):
                 delta[unit] = g_dash(inp[unit])*(output_vector[unit - no_input_nodes - no_hidden_nodes] - a[unit])
+                #print output_vector[unit - no_input_nodes - no_hidden_nodes]
+                #print a[unit]
+            #print output_vector[-4:]
+            #print delta
+            #exit()
             for unit in range(no_input_nodes, no_input_nodes + no_hidden_nodes):
                 delta[unit] = g_dash(inp[unit])*summation2(w, delta, unit, no_input_nodes + no_hidden_nodes, no_input_nodes + no_hidden_nodes + no_output_nodes)
             # for unit in range(no_input_nodes):
             #     delta[unit] = g_dash(inp[unit])*summation2(w, delta, unit, no_input_nodes, no_input_nodes + no_hidden_nodes)
 
             #Update Weights
-            w = update_weights(w, 0.8, a, delta, no_input_nodes, no_hidden_nodes, no_output_nodes)
+            w = update_weights(w, 0.05, a, delta, no_input_nodes, no_hidden_nodes, no_output_nodes)
             print "Test " + str(counter) + " done"
+        print cnt
         print "Accuracy="+str(cnt/300)
     print w
 
@@ -116,6 +130,4 @@ def main():
         test_vector[line[0]] = (line[1], line[2:])
     ann(train_vector)
     print time.time() - start_time
-
-
 main()
