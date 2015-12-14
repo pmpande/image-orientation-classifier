c3240ce863026e66b52ec8c93cc5c00a4c68fc67
Pranav Pande <pmpande@iu.edu>
2015-12-14 00:03:50 -0500

    Updated for documentation mistake

--- orient.py
+++ orient.py
@@ -1,33 +1,40 @@
+# Names: 1. Pranav Pande (pmpande)
+#        2. Nishant Shah (nishshah)
+# We have implemented KNN and ANN in seperate files and calling their appropriate methods in this file.
+# Repository also includes trained weights and incorrect outputs from ANN using best configuration.
+# 1) KNN
+# In KNN we are taking Euclidian distance.
+# But for sake of less complexity in running time, we are not finding the square root for finding distance, and keeping
+# square value of it.
+# Training KNN takes a lot of time in local machine(Around 80-90 mins) as we haven't done any optimization.
+# 2) ANN
+# We started ANN with constant weight 1 to all weights but this resulted in convergence of fitness too early and also
+# for every output node.
+# So we explored resources to know what should be ideal initial weight distribution and what number of hidden layers
+# should be.
+# We then came up with a solution of initial weight to be random number between (-1/sqrt(i),1/sqrt(i)) where i is the
+# number of input nodes
+# and number of hidden nodes to be mean of input and output nodes. But this solution didn't work out well and required
+# long time for training.
+# So we went for trials and erros for initial weight distribution and number of hidden nodes.
+# All results of these experiments are well documented in PDF report.
+# Every training of ANN also produces a file trained_weights which eases multiple running of test for same configured
+# neural network.
+# ->trained_weights which contains weights of every edge.
+# Every test run produces a file "incorrect" which contains images classified incorrectly.
+# About calculation:
+# We are not using matrix multiplication or any external library to speed up our code.
+# Our single training completes running in 2-3 mins.
+# 3) best
+# For best case, we are using ANN. Configuration and result of it is explained in PDF report.
+# We have cached the weights of this ANN in model_file. So running the best program will test the test data instantly.
 import sys
 import time
 from k_nearest_neighbour import knn
 from neural_network_classifier import nnet
 from best_method import best
 start_time = time.time()
-'''
-We have implemented KNN and ANN in seperate files and calling their appropriate methods in this file.
-Repository also includes trained weights and incorrect outputs from ANN using best configuration.
-1) KNN
-In KNN we are taking Euclidian distance.
-But for sake of less complexity in running time, we are not finding the square root for finding distance, and keeping square value of it.
-Training KNN takes a lot of time in local machine(Around 80-90 mins) as we haven't done any optimization.
-2) ANN
-We started ANN with constant weight 1 to all weights but this resulted in convergence of fitness too early and also for every output node.
-So we explored resources to know what should be ideal initial weight distribution and what number of hidden layers should be.
-We then came up with a solution of initial weight to be random number between (−1/√i,1/√i) where i is the number of input nodes 
-and number of hidden nodes to be mean of input and output nodes. But this solution didn't work out well and required long time for training.
-So we went for trials and erros for initial weight distribution and number of hidden nodes. 
-All results of these experiments are well documented in PDF report.
-Every training of ANN also produces a file trained_weights which eases multiple running of test for same configured neural network.
-->trained_weights which contains weights of every edge.
-Every test run produces a file "incorrect" which contains images classified incorrectly.
-About calculation:
-We are not using matrix multiplication or any external library to speed up our code.
-Our single training completes running in 2-3 mins.
-3) best
-For best case, we are using ANN. Configuration and result of it is explained in PDF report.
-We have cached the weights of this ANN in model_file. So running the best program will test the test data instantly.
-'''
+
 
 def main(train_file, test_file, algorithm, param):
     # Dictionary to store data from training and test files
